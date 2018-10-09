29d7e0209dfce75c8d3a18859d6b89f898008f44
Vishnu Haridas <vishnuh655@gmail.com>
2018-10-09 21:13:53 +0530

    First app

---
+++ main.js
@@ -0,0 +1,23 @@
+const electron = require("electron");
+const { app, BrowserWindow } = require("electron");
+
+let win;
+
+function createWindow() {
+  // Create the browser window.
+  win = new BrowserWindow({ width: 800, height: 600 });
+
+  // and load the index.html of the app.
+  win.loadFile("index.html");
+  win.webContents.openDevTools();
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
