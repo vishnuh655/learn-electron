0b66d3fb5ec014d1eea399904af259f1a9189357
Vishnu Haridas <vishnuh655@gmail.com>
2018-10-10 16:53:22 +0530

    Final

--- main.js
+++ main.js
@@ -1,23 +1,105 @@
 const electron = require("electron");
-const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const url = require("url");
 
-let win;
+const { app, BrowserWindow, Menu, ipcMain } = electron;
 
-function createWindow() {
-  // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 });
+let mainWindow;
+let addWindow;
 
-  // and load the index.html of the app.
-  win.loadFile("index.html");
-  win.webContents.openDevTools();
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({});
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "mainWindow.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+
+  // Quit all windows on window clos
+  mainWindow.on("closed", () => {
+    app.quit();
+  });
+
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  Menu.setApplicationMenu(mainMenu);
+});
+
+// Handle Add window
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: "Shopping List Item"
+  });
+  addWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "addWindow.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+  //Garbage Collection Handle
+  addWindow.on("close", () => {
+    addWindow = null;
+  });
 }
 
-app.on("ready", createWindow);
-
-app.on("window-all-closed", () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+//Catch item:add
+ipcMain.on("item:add", (e, item) => {
+  console.log(item);
+  mainWindow.webContents.send("item:add", item);
+  addWindow.close();
 });
+
+const mainMenuTemplate = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Add Item",
+        click() {
+          createAddWindow();
+        }
+      },
+      {
+        label: "Clear Items",
+        click() {
+          mainWindow.webContents.send("item:clear");
+        }
+      },
+      {
+        label: "Exit",
+        accelerator: process.platform == "darwin" ? "Command+Q" : "Ctrl+Q",
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+
+//If mac add an empty object to menu
+if (process.platform == "darwin") {
+  mainMenuTemplate.unshift({});
+}
+
+//Add develpoer tools item if not in production
+if (process.env.NODE_ENV !== "production") {
+  mainMenuTemplate.push({
+    label: "DevTools",
+    submenu: [
+      {
+        label: "Chrome Dev Tool",
+        accelerator: process.platform == "darwin" ? "Command+I" : "Ctrl+I",
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        role: "reload"
+      }
+    ]
+  });
+}
